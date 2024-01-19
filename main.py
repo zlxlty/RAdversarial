e062ade3083c4ae150f86d86916e08a3c4911261
zlxlty <lus2@carleton.edu>
2024-01-19 11:31:09 -0600

    feat: restructure model code, implement maximization method

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from classifiers import get_target_model
-from attacks import do_perturbation, do_locsearchadv
+from attacks import do_perturbation
 from PIL import Image
 import requests
-import torch
 import torch.nn as nn
 
 # values are standard normalization for ImageNet images, 
@@ -12,20 +11,16 @@ if __name__ == '__main__':
     # testing set can just be urls to images in coco dataset
     # url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     # image = Image.open(requests.get(url, stream=True).raw)
-    pig_img = Image.open("./images/pig.jpg")
+    pig_img = Image.open("./images/final_image.jpg")
     
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    
-    target_model = get_target_model("MobileViT", device)
+    target_model = get_target_model("MobileViT")
     input_tensor = target_model.preprocess(pig_img)
+    print(input_tensor)
     
-    # torch.save(input_tensor, 'input_img.pt')
-    # input_tensor = torch.load('input_img.pt')
-    # do_locsearchadv(input_tensor, 10, 1, 3, 5, 5, 1, target_model)
-    do_perturbation(input_tensor, 341, target_model)
+    # do_perturbation(input_tensor, 341, target_model)
     
-    # logit = target_model.predict(input_tensor)
-    # max_class = logit.max(dim=1)[1].item()
-    # print("Predicted class: ", target_model.id2label(max_class))
-    # print("Predicted probability:", nn.Softmax(dim=1)(logit)[0, max_class].item())
+    logit = target_model.predict(input_tensor)
+    max_class = logit.max(dim=1)[1].item()
+    print("Predicted class: ", target_model.id2label(max_class))
+    print("Predicted probability:", nn.Softmax(dim=1)(logit)[0, max_class].item())
     
