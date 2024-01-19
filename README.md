96096649a39f3f0be61625ee1fd2d874d2606ac9
Peter Tu <tut@carleton.edu>
2024-01-19 14:37:28 -0600

    Add Readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# How to run
+
+**You only need to run it once.**
+
+```bash
+docker build . -t comps
+```
+
+For each time you want to use docker
+
+```bash
+sh run.sh
+```
--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-docker build . -t comps
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it -v $(pwd):/my-repo --rm comps
+docker run --gpus all \
+    --ipc=host --ulimit memlock=-1 \
+    --ulimit stack=67108864 \
+    -it \
+    -v $(pwd):/my-repo \
+    -w /my-repo \
+    --rm comps
