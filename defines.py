fc077ba278042ab89d67371aa0578a292da50a41
zlxlty <lus2@carleton.edu>
2024-02-07 13:17:26 -0600

    add: dataset loader

--- defines.py
+++ defines.py
@@ -6,3 +6,4 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 EVAL_PATH = f'{ROOT_DIR}/evals'
 IMAGE_PATH = f'{ROOT_DIR}/images'
 CONFIG_PATH = f'{ROOT_DIR}/attacks/config'
+DATASET_PATH = f'/dataset/benchmark'
