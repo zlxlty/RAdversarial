f3a593785d1bced5e1ee0bd4b54bd76b3bdeb1d8
zlxlty <lus2@carleton.edu>
2024-02-15 23:28:28 -0600

    update: ep 2 evals; refactor: config param loading

--- attacks/attack_method.py
+++ attacks/attack_method.py
@@ -8,53 +8,65 @@ import os
 import sys
 
 import yaml
+
 sys.path.append("..")
 from classifiers import TargetModel, label2id, id2label
 from .utils import create_folders
 
-class AttackMethod():
-    def __init__(self, model: TargetModel, config_file: str):
+
+class AttackMethod:
+    def __init__(self, model: TargetModel, config_dict: dict):
         self.model = model
         self.perturbed_input = None
         self.logit = None
         self.number_iteration = None
-        
-        with open(config_file, 'r') as file:
-            data = yaml.safe_load(file)
-        self.param_config = data
-            
+        self.param_config = config_dict
 
-    def do_perturbation(self, input_tensor: torch.Tensor, label: int) -> 'AttackMethod':
-        '''
+    def do_perturbation(self, input_tensor: torch.Tensor, label: int) -> "AttackMethod":
+        """
         Inherit this class to implement the actual perturbation method
-        '''
+        """
         raise NotImplementedError
-    
-    def do_eval(self, org_img_tensor: torch.Tensor, true_label_idx: int, topk: int = 5, true_target_model = None) -> 'AttackMethod':
+
+    def do_eval(
+        self,
+        org_img_tensor: torch.Tensor,
+        true_label_idx: int,
+        topk: int = 5,
+        true_target_model=None,
+    ) -> "AttackMethod":
         if self.logit is None:
             raise Exception("Must call do_perturbation first")
-        
+
         if topk < 1 or topk > 5:
             raise Exception("topk must be between 1 and 5")
-        
+
         # Surrogate models are evaluated on the true target model (MobileViT)
         if true_target_model != None:
             self.model = true_target_model
-            org_img_tensor = F.interpolate(org_img_tensor, size=(256, 256), mode='bilinear', align_corners=False)
-            self.perturbed_input = F.interpolate(org_img_tensor, size=(256, 256), mode='bilinear', align_corners=False)
+            org_img_tensor = F.interpolate(
+                org_img_tensor, size=(256, 256), mode="bilinear", align_corners=False
+            )
+            self.perturbed_input = F.interpolate(
+                org_img_tensor, size=(256, 256), mode="bilinear", align_corners=False
+            )
             self.logit = self.model.predict(self.perturbed_input)
-        
+
         # Original Image Prob
         pred = self.model.predict(org_img_tensor)
-        
+
         # get top 1 prediction from pred
         self.original_top1_index = torch.argmax(pred, dim=1).item()
-        self.original_prediction_result = nn.Softmax(dim=1)(pred)[0, true_label_idx].item()
-        
+        self.original_prediction_result = nn.Softmax(dim=1)(pred)[
+            0, true_label_idx
+        ].item()
+
         # # Top 1 accuracy
-        self.true_class_probability = nn.Softmax(dim=1)(self.logit)[0, true_label_idx].item()
+        self.true_class_probability = nn.Softmax(dim=1)(self.logit)[
+            0, true_label_idx
+        ].item()
         print("True class probability:", self.true_class_probability)
-        
+
         # Top k accuracy
         topk_indices = torch.topk(self.logit, topk, dim=1)[1].squeeze().tolist()
         topk_indices = [topk_indices] if topk == 1 else topk_indices
@@ -62,27 +74,31 @@ class AttackMethod():
         topk_probabilities = nn.Softmax(dim=1)(self.logit)[0, topk_indices].tolist()
         print(f"Predicted top {topk} classes: ", topk_labels)
         print(f"Predicted top {topk} probabilities:", topk_probabilities)
-        
+
         self.topk_indices = topk_indices
         self.topk_labels = topk_labels
         self.topk_probabilities = topk_probabilities
-        
+
         return self
-    
-    def save_eval_to_json(self, input_name, true_label_idx, filename: str) -> 'AttackMethod':
-        if self.topk_indices is None\
-            or self.topk_labels is None\
-            or self.topk_probabilities is None\
-            or self.true_class_probability is None:
+
+    def save_eval_to_json(
+        self, input_name, true_label_idx, filename: str
+    ) -> "AttackMethod":
+        if (
+            self.topk_indices is None
+            or self.topk_labels is None
+            or self.topk_probabilities is None
+            or self.true_class_probability is None
+        ):
             raise Exception("Must call do_eval first")
-        
+
         if filename[-5:] != ".json":
             raise Exception("Filename must end with .json")
-        
+
         create_folders(filename)
-        
+
         import json
-        
+
         json_dict = {
             "input_name": input_name,
             "true_label": id2label(true_label_idx),
@@ -92,65 +108,66 @@ class AttackMethod():
             "true_class_probability": self.true_class_probability,
             "topk_indices": self.topk_indices,
             "topk_labels": self.topk_labels,
-            "topk_probabilities": self.topk_probabilities
+            "topk_probabilities": self.topk_probabilities,
         }
-        
+
         ## Only for LocSearchAdv
         if self.number_iteration is not None:
             json_dict["num_iteration"] = self.number_iteration
-        
+
         result_list = None
         if os.path.exists(filename):
-            with open(filename, 'r') as f:
+            with open(filename, "r") as f:
                 result_list = json.load(f)
-            
+
         if isinstance(result_list, list):
-            with open(filename, 'w') as f:
+            with open(filename, "w") as f:
                 result_list.append(json_dict)
                 json.dump(result_list, f)
         else:
-            with open(filename, 'w') as f:
+            with open(filename, "w") as f:
                 json.dump([json_dict], f)
-                
-    
+
         return self
-    
-    def save_perturbation_to_json(self, filename: str) -> 'AttackMethod':
+
+    def save_perturbation_to_json(self, filename: str) -> "AttackMethod":
         if self.perturbed_input is None:
             raise Exception("Must call do_perturbation first")
-        
+
         if filename[-5:] != ".json":
             raise Exception("Filename must end with .json")
-        
+
         create_folders(filename)
-        
+
         import json
-        
-        json_dict = {
-            "perturbed_input": self.perturbed_input.tolist()
-        }
-        
-        with open(filename, 'w') as f:
+
+        json_dict = {"perturbed_input": self.perturbed_input.tolist()}
+
+        with open(filename, "w") as f:
             json.dump(json_dict, f)
-            
+
         return self
-    
-    def save_perturbation_to_png(self, filename: str) -> 'AttackMethod':
+
+    def save_perturbation_to_png(self, filename: str) -> "AttackMethod":
         if self.perturbed_input is None:
             raise Exception("Must call do_perturbation first")
-        
+
         if filename[-4:] != ".png":
             raise Exception("Filename must end with .png")
-        
+
         create_folders(filename)
 
-        final_image = self.perturbed_input.squeeze().detach().cpu()  # Remove the batch dimension and move to CPU
-        rgb_image = cv2.cvtColor(((final_image)*255).detach().numpy().transpose(1, 2, 0), cv2.COLOR_BGR2RGB)
+        final_image = (
+            self.perturbed_input.squeeze().detach().cpu()
+        )  # Remove the batch dimension and move to CPU
+        rgb_image = cv2.cvtColor(
+            ((final_image) * 255).detach().numpy().transpose(1, 2, 0), cv2.COLOR_BGR2RGB
+        )
 
         # Convert the numpy array to a PIL image
-        pil_image = Image.fromarray(rgb_image.astype('uint8'))
+        pil_image = Image.fromarray(rgb_image.astype("uint8"))
 
         # Save the image as png
         pil_image.save(filename)
-        
+
         return self
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-from classifiers import get_target_model, label2id, VGG16
-from attacks import PGDMethod, FGSMMethod, NoMethod
 from PIL import Image
 import torch
 import os
+import yaml
 
 from defines import IMAGE_PATH, EVAL_PATH, CONFIG_PATH, DATASET_PATH
-from classifiers import get_target_model, label2id
+from classifiers import get_target_model, label2id, VGG16
 from attacks import PGDMethod, FGSMMethod, LocSearchAdv, NoMethod
 
 
-# values are standard normalization for ImageNet images, 
+# values are standard normalization for ImageNet images,
 # from https://github.com/pytorch/examples/blob/master/imagenet/main.py
 def generate_image_data(skip=0):
-    '''
+    """
     [dataset]: Get image data from ImageNet1k folder
     Returns:
         image: PIL.Image
         true_label_idx: int
-    '''
+    """
     image_folder = f"{DATASET_PATH}/images"
     label_txt = f"{DATASET_PATH}/labels.txt"
     with open(label_txt, "r") as f:
-        name2label = {line.split(": ")[0]: line.split(": ")[1] for line in f.readlines()}
-    
+        name2label = {
+            line.split(": ")[0]: line.split(": ")[1] for line in f.readlines()
+        }
+
     # iterate and open each image file in image folder
     skipped = 0
     for image_name in os.listdir(image_folder):
@@ -38,62 +39,62 @@ def create_dir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
+
 attack_methods = {
     "LocSearchAdv": {
         "config": f"{CONFIG_PATH}/locsearchadv.yaml",
-        "method": LocSearchAdv
+        "method": LocSearchAdv,
     },
-    "FGSM": {
-        "config": f"{CONFIG_PATH}/fgsm.yaml",
-        "method": FGSMMethod
-    },
-    "PGD": {
-        "config": f"{CONFIG_PATH}/pgd.yaml",
-        "method": PGDMethod
-    },
-    "NO": {
-        "config": f"{CONFIG_PATH}/no.yaml",
-        "method": NoMethod
-    },
+    "FGSM": {"config": f"{CONFIG_PATH}/fgsm.yaml", "method": FGSMMethod},
+    "PGD": {"config": f"{CONFIG_PATH}/pgd.yaml", "method": PGDMethod},
+    "NO": {"config": f"{CONFIG_PATH}/no.yaml", "method": NoMethod},
 }
 
-'''
+"""
 Choose the target models and attack methods here.
-'''
+"""
 TARGET_MODEL = [
-    # "MobileViT", 
-    # "Surrogate", 
-    "ResNet50"
+    "MobileViT",
+    # "Surrogate",
+    "ResNet50",
 ]
 METHOD_NAMES = [
-    "PGD", 
-    "FGSM", 
+    "PGD",
+    "FGSM",
     # "NO"
 ]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    TARGETxMETHOD = [(target, method) for target in TARGET_MODEL for method in METHOD_NAMES]
-    
+    TARGETxMETHOD = [
+        (target, method) for target in TARGET_MODEL for method in METHOD_NAMES
+    ]
+
     for model_name, method_name in TARGETxMETHOD:
         target_model = get_target_model(model_name, device)
         true_target_model = None
         if model_name == "Surrogate":
             true_target_model = get_target_model("MobileViT", device)
-        
+
         attack = attack_methods[method_name]
         config_path = attack["config"]
-        image_data_generator = generate_image_data(skip=13)
+        with open(config_path, "r") as file:
+            config_dict = yaml.safe_load(file)
+
+        image_data_generator = generate_image_data(skip=0)
         image_processed = 0
         for image_name, original_image, true_label in image_data_generator:
             input_tensor = target_model.preprocess(original_image)
             true_label_idx = label2id(true_label)
-            attack["method"](target_model, config_path)\
-                .do_perturbation(input_tensor, true_label_idx)\
-                .do_eval(input_tensor, true_label_idx, 5, true_target_model)\
-                .save_eval_to_json(image_name, true_label_idx, f"{EVAL_PATH}/{method_name}/{method_name}_{model_name}.json")\
-                # .save_perturbation_to_json(f"{IMAGE_PATH}/{method_name}/{model_name}/perturbed_{image_name[:-5]}.json")\
-                # .save_perturbation_to_png(f"{IMAGE_PATH}/{method_name}/{model_name}/perturbed_{image_name[:-5]}.png")
+            attack["method"](target_model, config_dict).do_perturbation(
+                input_tensor, true_label_idx
+            ).do_eval(
+                input_tensor, true_label_idx, 5, true_target_model
+            ).save_eval_to_json(
+                image_name,
+                true_label_idx,
+                f"{EVAL_PATH}/{method_name}/{method_name}_{model_name}_{config_dict['epsilon'][0]}ep.json",
+            )  # .save_perturbation_to_json(f"{IMAGE_PATH}/{method_name}/{model_name}/perturbed_{image_name[:-5]}.json")\
+            # .save_perturbation_to_png(f"{IMAGE_PATH}/{method_name}/{model_name}/perturbed_{image_name[:-5]}.png")
             image_processed += 1
             print(f"Image processed: {image_processed}")
-
